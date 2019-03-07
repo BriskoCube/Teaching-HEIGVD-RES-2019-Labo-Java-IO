e2814f75b9318ff07d2b5ab9bc96e740c3098941
Julien Quartier <julien.quartier@bluewin.ch>
2019-03-07 08:38:04 +0100

    File exploration implemented

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -9,14 +9,26 @@ import java.io.File;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
+    // loop on files
+    for (File file : rootDirectory.listFiles()) {
+      if (!file.isDirectory()) {
+        vistor.visit(file);
+      }
+    }
+
+    // loop on directories
+    for (File file : rootDirectory.listFiles()) {
+      if (file.isDirectory()) {
+        explore(file, vistor);
+      }
+    }
+  }
 }
